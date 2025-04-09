72da944221938e06b4f1c986e4e6f2601cc019d1
ArcticStranger <159117327+ArcticStranger@users.noreply.github.com>
2025-04-09 17:35:23 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+const logoGif = document.querySelector('.logo-gif');
+const emailInputs = document.querySelectorAll('.input-box input[type="email"]');
+const passwordInputs = document.querySelectorAll('.input-box input[type="password"]');
+const newSrc = "./mainmenuassets/eye.gif";
+const originalSrc = "./mainmenuassets/arasaka.gif";
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
+
+logoGif.addEventListener('click', () => {
+    if (logoGif.src !== newSrc) {
+        logoGif.src = newSrc;
+        logoGif.classList.add('new-image'); 
+               // Меняем тип у ВСЕХ полей пароля
+        passwordInputs.forEach(input => {
+            input.type = "text";
+        });
+    } else {
+        logoGif.src = originalSrc;
+        logoGif.classList.remove('new-image'); 
+             // Возвращаем тип обратно
+        passwordInputs.forEach(input => {
+            input.type = "password";
+        });
+    }
+});
